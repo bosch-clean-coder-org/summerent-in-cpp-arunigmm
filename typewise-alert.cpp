2e81aabc0f60398fba3e5862de0691a09c7c91c4
arunigmm <138025039+arunigmm@users.noreply.github.com>
2023-07-06 15:41:31 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -6,12 +6,13 @@ typedef std::pair<int,int> BoundValues;
 
 std::map<CoolingType, BoundValues> CoolingBoundValue = 
 { 
-{std::make_pair( PASSIVE_COOLING, std::make_pair(0,35))},
-{std::make_pair( HI_ACTIVE_COOLING, std::make_pair(0,45))},
-{std::make_pair( MED_ACTIVE_COOLING, std::make_pair(0,40))}
+   {std::make_pair( PASSIVE_COOLING,    std::make_pair(0,35))},
+   {std::make_pair( HI_ACTIVE_COOLING,  std::make_pair(0,45))},
+   {std::make_pair( MED_ACTIVE_COOLING, std::make_pair(0,40))}
 };
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
   if(value < lowerLimit) {
     return TOO_LOW;
   }
@@ -21,21 +22,17 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) 
-  {
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
   BoundValues temp = CoolingBoundValue[coolingType];
   return inferBreach(temperatureInC, temp.first, temp.second);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
-
-  switch(alertTarget) {
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
+{
+  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+  switch(alertTarget) 
+  {
     case TO_CONTROLLER:
       sendToController(breachType);
       break;
@@ -45,14 +42,17 @@ void checkAndAlert(
   }
 }
 
-void sendToController(BreachType breachType) {
+void sendToController(BreachType breachType) 
+{
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
 }
 
-void sendToEmail(BreachType breachType) {
+void sendToEmail(BreachType breachType) 
+{
   const char* recepient = "a.b@c.com";
-  switch(breachType) {
+  switch(breachType) 
+  {
     case TOO_LOW:
       printf("To: %s\n", recepient);
       printf("Hi, the temperature is too low\n");
