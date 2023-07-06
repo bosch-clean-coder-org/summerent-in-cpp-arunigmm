81dc6fb11e33dfd07664048caae0444bb9123c18
arunigmm <138025039+arunigmm@users.noreply.github.com>
2023-07-06 16:24:20 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -4,17 +4,23 @@
 
 typedef std::pair<int,int> BoundValues;
 
-std::map<CoolingType, BoundValues> CoolingBoundValue = 
+std::map<CoolingType, BoundValues> coolingBoundValue = 
 { 
-   {std::make_pair( PASSIVE_COOLING,    std::make_pair(0,35))},
-   {std::make_pair( HI_ACTIVE_COOLING,  std::make_pair(0,45))},
-   {std::make_pair( MED_ACTIVE_COOLING, std::make_pair(0,40))}
+   {std::make_pair(PASSIVE_COOLING,    std::make_pair(0,35))},
+   {std::make_pair(HI_ACTIVE_COOLING,  std::make_pair(0,45))},
+   {std::make_pair(MED_ACTIVE_COOLING, std::make_pair(0,40))}
 };
 
+std::map<BreachType, std::string> breachMessage =
+{
+   {std::make_pair(NORMAL,    "Hi, the temperature is Normal")},
+   {std::make_pair(TOO_LOW,   "Hi, the temperature is Too Low")},
+   {std::make_pair(TOO_HIGH,  "Hi, the temperature is Too High")}
+};
 
 BreachType inferTemperatureBreach(CoolingType coolingType, double temperatureInC) 
 {
-   BoundValues temperatureRange = CoolingBoundValue[coolingType];
+   BoundValues temperatureRange = coolingBoundValue[coolingType];
    if(temperatureInC < temperatureRange.first) 
    {
       return TOO_LOW;
@@ -48,16 +54,7 @@ void sendToController(BreachType breachType)
 
 void sendToEmail(BreachType breachType) 
 {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) 
-  {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-  }
+   const char* recepient = "a.b@c.com";
+   printf("To: %s\n", recepient);
+   printf("Message: %s\n", breachMessage[breachType]);
 }
