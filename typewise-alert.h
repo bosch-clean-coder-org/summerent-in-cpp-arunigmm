926453a5ea465d7994a27b402ca93a6baab7cb80
arunigmm <138025039+arunigmm@users.noreply.github.com>
2023-07-06 16:02:12 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -29,8 +29,7 @@ typedef struct {
 } BatteryCharacter;
 
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+BreachType inferTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 
 void checkAndAlert( AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
